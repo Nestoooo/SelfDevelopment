b6b6f438ff4ebeed384582e8432b9060a15d06ee
Nestoooo <70722194+Nestoooo@users.noreply.github.com>
2022-02-03 11:27:45 +0100

    new updates

---
+++ SelfDevelopment/base/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from .models import Room, Message
+
+#room model to use it when creating or updating a room
+class RoomForm(ModelForm):
+    class Meta:
+        model = Room
+        fields = '__all__'
+
+class MessageForm(ModelForm):
+    class Meta:
+        model = Message
+        fields = '__all__'
